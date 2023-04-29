68711fc9eea2e9dbcc65713327083c982c4dc783
Clemens Eisenhofer <Clemens.Eisenhofer@tuwien.ac.at>
2023-04-30 01:06:33 +0200

    Multithreading for running benchmarks

--- src/cli/main.cpp
+++ src/cli/main.cpp
@@ -2,8 +2,10 @@
 #include <iterator>
 #include <filesystem>
 #include <fstream>
+#include <semaphore>
 #include <set>
 #include <string_view>
+#include <thread>
 #include <vector>
 #include <z3++.h>
 
@@ -14,16 +16,93 @@
 #include "parse_exception.h"
 
 const char* HELP =
-" [-m=size_in_mb] [-t=timeout_in_ms] [-r] [-l=log_file] Mode File\n"
+" [-m=size_in_mb] [-t=timeout_in_ms] [-r] [-l=log_file] [-tr=thread_cnt] Mode File\n"
 "Solves a multi-modal formula\n\n"
 "Mode\t one of: std (std translation), id (iterative deepening), id2 (iterative deepening with unrolling), upl (user-propagator; lazy) [use \"upl\"; the other are for comparison only]\n"
 "File\t Path to an SMTLIB2 set_is_benchmark\n";
+
+void run(std::string path, unsigned idx, std::vector<std::string>* results,
+         std::counting_semaphore<100>* free_threads,
+         std::string_view mode, unsigned limit_time, unsigned limit_mem) {
+
+    std::cout << "Input: " << path << std::endl;
+    z3::context ctx;
+    modal_decls decls = modal_decls::create_default(ctx);
+
+    z3::expr_vector parsed = z3::expr_vector(ctx);
+    try {
+        parsed = ctx.parse_file(path.c_str(), decls.get_sorts(), decls.get_decls());
+    }
+    catch (const exception& e) {
+        std::cerr << "Parsing failed: " << e.msg() << std::endl;
+        free_threads->release();
+        exit(EXIT_FAILURE);
+    }
+
+    auto id  = [&decls](z3::context& ctx) { return new iterative_deepening_quant(ctx, decls); };
+    auto id2 = [&decls](z3::context& ctx) { return new iterative_deepening_unrolled(ctx, decls); };
+    auto std = [&decls](z3::context& ctx) { return new standard_translation(ctx, decls); };
+    auto upl = [&decls](z3::context& ctx) { return new lazy_up(ctx, decls); };
+
+    std::unordered_map<std::string_view, std::function<strategy*(z3::context&)>> mapping =
+    {
+        { "id",  id  },
+        { "id2",  id2  },
+        { "std", std },
+        { "upl", upl },
+    };
+
+    auto it = mapping.find(mode);
+    if (it == mapping.end()) {
+        std::cerr << "unknown mode: " << mode << std::endl;
+        std::cerr << HELP;
+        free_threads->release();
+        exit(EXIT_FAILURE);
+    }
+    strategy* str = it->second(ctx);
+    str->set_is_benchmark(true);
+    if (limit_time)
+        str->set_timeout(limit_time);
+    if (limit_mem)
+        str->set_memout(limit_mem);
+    (*results)[idx] = path + ";";
+    try {
+        check_result res = str->check(z3::mk_and(parsed));
+        str->output_state(std::cout);
+        if (res == z3::unknown)
+            (*results)[idx] += "-1;-1\n";
+        else if (res == z3::unsat)
+            (*results)[idx] += std::to_string(str->solving_time().count()) + ";0\n";
+        else
+            (*results)[idx] += std::to_string(str->solving_time().count()) + ";" + std::to_string(str->domain_size()) + "\n";
+
+        std::cout << "Solving-Time: " << str->solving_time().count() << std::endl;
+
+        if (mode == "upl" && res == z3::sat) {
+            if (str->model_check(z3::mk_and(parsed)) != Z3_L_TRUE) {
+                std::cout << "FAILED to check model" << std::endl;
+            }
+            else {
+                std::cout << "SUCCESSFULLY checked model" << std::endl;
+            }
+        }
+    } catch (const parse_exception& e) {
+        std::cerr << "Error: " << e.what() << std::endl;
+        (*results)[idx] += "Crashed!\n";
+        delete str;
+        free_threads->release();
+        exit(EXIT_FAILURE);
+    }
+    free_threads->release();
+    delete str;
+}
 
 int main(int argc, char** argv) {
 
     unsigned start = 1;
     unsigned limit_mem = 0;
     unsigned limit_time = 0;
+    unsigned threads = 1;
     std::string log_file;
     bool recursive = false;
     for (; start < argc; start++) {
@@ -33,6 +112,10 @@ int main(int argc, char** argv) {
         }
         if (strncmp(argv[start], "-t=", 3) == 0) {
             limit_time = atoi(argv[start] + 3);
+            if (limit_time < 1) {
+                std::cerr << "the time limit must be non-negative" << std::endl;
+                return -1;
+            }
             continue;
         }
         if (strncmp(argv[start], "-r", 2) == 0) {
@@ -45,6 +128,19 @@ int main(int argc, char** argv) {
                 log_file = log_file.substr(1);
             if (log_file.ends_with('"'))
                 log_file.pop_back();
+            continue;
+        }
+        if (strncmp(argv[start], "-tr=", 4) == 0) {
+            log_file = argv[start] + 4;
+            threads = atoi(argv[start] + 4);
+            if (threads < 1) {
+                std::cerr << "the number of threads has to be positive" << std::endl;
+                return -1;
+            }
+            if (threads > 100) {
+                std::cerr << "the number of threads must be at most 100" << std::endl;
+                return -1;
+            }
             continue;
         }
         break;
@@ -100,112 +196,29 @@ int main(int argc, char** argv) {
             return -1;
         }
     }
-    
+
+    std::vector<std::string> results;
+    results.resize(paths.size());
+    unsigned idx = 0;
+
+    std::vector<std::thread> thread_list;
+    std::counting_semaphore<100> free_threads(threads);
+
     for (const std::string& input : paths) {
-    
-        std::cout << "Input: " << input << std::endl; 
-
-        /*Z3_enable_trace("rewriter");
-        Z3_enable_trace("rewriter_visit");
-        Z3_enable_trace("rewriter_subst");
-        Z3_enable_trace("reduce_app");
-        Z3_enable_trace("reduce_quantifier");
-        Z3_enable_trace("rewriter_reuse");
-        Z3_enable_trace("rewriter_step");
-        Z3_enable_trace("elim_unused_vars");*/
-        
-        //z3::set_param("sat.euf", true);
-        //z3::set_param("sat.smt", true);
-        z3::context ctx;
-        modal_decls decls = modal_decls::create_default(ctx);
-        
-        z3::expr_vector parsed = z3::expr_vector(ctx);
-        try {
-            parsed = ctx.parse_file(input.c_str(), decls.get_sorts(), decls.get_decls());
-        }
-        catch (const exception& e) {
-            std::cerr << "Parsing failed: " << e.msg() << std::endl;
-            continue;
-        }
-    
-        auto id  = [&decls](z3::context& ctx) { return new iterative_deepening_quant(ctx, decls); };
-        auto id2 = [&decls](z3::context& ctx) { return new iterative_deepening_unrolled(ctx, decls); };
-        auto std = [&decls](z3::context& ctx) { return new standard_translation(ctx, decls); };
-        auto upl = [&decls](z3::context& ctx) { return new lazy_up(ctx, decls); };
-    
-        std::unordered_map<std::string_view, std::function<strategy*(z3::context&)>> mapping =
-        {
-            { "id",  id  },
-            { "id2",  id2  },
-            { "std", std },
-            { "upl", upl },
-        };
-    
-        auto it = mapping.find(mode);
-        if (it == mapping.end()) {
-            std::cerr << "unknown mode: " << mode << std::endl;
-            std::cerr << HELP;
-            return EXIT_FAILURE;
-        }
-        strategy* str = it->second(ctx);
-        str->set_is_benchmark(true);
-        if (limit_time)
-            str->set_timeout(limit_time);
-        if (limit_mem)
-            str->set_memout(limit_mem);
-        try {
-            if (out)
-                *out << input.c_str() << ";";
-            check_result res = str->check(z3::mk_and(parsed));
-            str->output_state(std::cout);
-            if (out) {
-                if (res == z3::unknown)
-                    *out << "-1;-1\n";
-                else if (res == z3::unsat)
-                    *out << str->solving_time().count() << ";0\n";
-                else
-                    *out << str->solving_time().count() << ";" << str->domain_size() << "\n";
-            }
-            std::cout << "Solving-Time: " << str->solving_time().count() << std::endl;
-
-            if (mode == "upl" && res == z3::sat) {
-                if (str->model_check(z3::mk_and(parsed)) != Z3_L_TRUE) {
-                    std::cout << "FAILED to check model" << std::endl;
-                }
-                else {
-                    std::cout << "SUCCESSFULLY checked model" << std::endl;
-                }
-            }
-        } catch (const parse_exception& e) {
-            delete str;
-            std::cerr << "Error: " << e.what() << std::endl;
-            if (out) {
-                *out << "Crashed!\n";
-                delete out;
-            }
-            return -1;
-        }
-
-#if 0
-        // For comparison
-        standard_translation check(ctx, decls);
-        check_result res_comp = check.check(z3::mk_and(parsed));
-        if (res != res_comp) {
-            std::cerr << "Different results: "
-            << (res == check_result::sat ? "sat" : res == check_result::unsat ? "unsat" : "unknown" )
-            << " vs. "
-            << (res_comp == check_result::sat ? "sat" : res_comp == check_result::unsat ? "unsat" : "unknown" ) << std::endl;
-        }
-        else {
-            std::cout << "Results coincide" << std::endl;
-        }
-#endif
-        
-        delete str;
+        free_threads.acquire();
+        std::thread t(run, input, idx, &results, &free_threads, mode, limit_time, limit_mem);
+        thread_list.push_back(std::move(t));
+        idx++;
     }
 
-    if (out)
+    for (unsigned i = 0; i < thread_list.size(); i++)
+        thread_list[i].join();
+
+    if (out) {
+        for (const auto& result : results)
+            *out << result;
         delete out;
+    }
 
     return 0;
 }
